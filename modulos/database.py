8e83fb5878c73c3fc166f9f16d57a5225149edcd
rfreir3 <rafael.oliveira835502@gmail.com>
2024-01-30 20:13:20 -0300

    Integração do sistema com o banco de dados (envio de dados)

--- modulos/database.py
+++ modulos/database.py
@@ -1,15 +1,13 @@
 import sqlite3
 
-connect = sqlite3.connect("meus_modulos\\SistemaDeBiblioteca.db")
+with sqlite3.connect("modulos\\SistemaDeBiblioteca.db") as connect:
+    cursor = connect.cursor()
 
-cursor = connect.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(         
-    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    Username TEXT NOT NULL,
-    Email TEXT NOT NULL,
-    Password TEXT NOT NULL     
-)""")
-
-print("Conexão feita com sucesso")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(         
+        Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        Username TEXT NOT NULL,
+        Email TEXT NOT NULL,
+        Password TEXT NOT NULL     
+    )""")
+    print("Conexão feita com sucesso")
--- modulos/register.py
+++ modulos/register.py
@@ -1,9 +1,27 @@
-from modulos.janela import login_entry, senha_entry, email_entry
-import modulos.database as database
+import sqlite3
 
+def insert_into_database(login, email, senha):
+    try:
+        # Conecta ao banco de dados
+        with sqlite3.connect("modulos\\SistemaDeBiblioteca.db") as connection:
+            # Cria um cursor a partir da conexão
+            cursor = connection.cursor()
 
-database.cursor.execute("""
-    INSERT INTO users VALUES(login_entry, senha_entry, email_entry)
-)""")
+            # Executa a operação no banco de dados
+            cursor.execute("INSERT INTO users (Username, Email, Password) VALUES (?, ?, ?)", (login, email, senha))
 
-database.connect.commit()
+            # Executa uma consulta SQL
+            cursor.execute("SELECT * FROM users")
+
+            # Recupera todos os resultados da consulta
+            resultados = cursor.fetchall()
+
+            # Imprime os resultados
+            for linha in resultados:
+                print(linha)
+
+            # Commit para salvar as alterações
+            connection.commit()
+
+    except sqlite3.Error as e:
+        print(f"Erro ao inserir dados no banco de dados: {e}")
